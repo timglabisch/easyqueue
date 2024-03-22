f0a9bc4b3fed8dbfa4120dc4df4d3cb1ec899646
timglabisch <glabisch@easybill.de>
2024-03-22 14:19:06 +0100

    ...

--- src/raft/mod.rs
+++ src/raft/mod.rs
@@ -6,18 +6,3 @@ pub mod raft;
 pub mod raft_node;
 pub mod raft_service;
 pub mod raft_server;
-
-use std::time::Duration;
-use tokio::task::JoinHandle;
-use tracing::{info, instrument};
-
-pub struct EbRaft;
-
-impl EbRaft {
-
-    #[instrument]
-    async fn tick() {
-        info!("tick");
-    }
-
-}
